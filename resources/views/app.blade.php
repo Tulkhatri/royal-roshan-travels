887da2a9358693e12aaf7bd73c97b47c47c87e7f
Tulkhatri <tulkhatri01@gmail.com>
2024-10-27 15:37:19 +0545

    fixed favicon image issue and styling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
-    @inertiaHead
-    @routes
-  </head>
-  <body>
-    @inertia
-  </body>
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <!-- Link to the new favicon -->
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/x-icon" />
+        @vite('resources/js/app.js')
+        @inertiaHead
+        @routes
+    </head>
+
+    <body>
+        @inertia
+    </body>
+
 </html>
