28ad99cbbd7bd883791207efa62325219b5ff70c
Tulkhatri <tulkhatri01@gmail.com>
2024-10-27 20:23:27 +0545

    fixed head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,12 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <meta property="og:title" content="Royal Roshan Travels and Tours">
+        <meta property="og:description" content="Explore the best tours and travel packages with us!">
+        <meta property="og:image" content="https://royalroshantravelsandtours.com.np/assets/images/roshan.jpeg">
+        <meta property="og:url" content="https://royalroshantravelsandtours.com.np">
+        <meta property="og:type" content="website">
+
         <!-- Link to the new favicon -->
         <link rel="icon" href="{{ asset('favicon.png') }}" type="image/x-icon" />
         @vite('resources/js/app.js')
